b62595a3960fbbb2731f005cf1a03f24bbda9ab3
annasovaa <anna.kurbanova404@gmail.com>
2026-01-16 20:19:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,13 +5,16 @@ form.addEventListener('submit', e => {
 
   const data = Object.fromEntries(new FormData(form));
 
-  fetch('ВАШ_WEB_APP_URL', {
+  fetch('https://script.google.com/macros/s/AKfycbwjUcmNO6qIL76AUnCd8311N02TwIUlDTfCdHxG5E1_qtK6h34dZgI_Si17-AZPEggy/exec', {
     method: 'POST',
     body: JSON.stringify(data)
   })
   .then(() => {
-    alert('Данные отправлены');
+    alert('Данные успешно отправлены');
     form.reset();
   })
-  .catch(() => alert('Ошибка отправки'));
+  .catch(error => {
+    console.error(error);
+    alert('Ошибка при отправке данных');
+  });
 });
